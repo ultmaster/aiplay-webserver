7f3c5433505f13e2384142de1b6bae6cb36932be
ultmaster <scottyugochang@hotmail.com>
2017-01-28 19:08:27 +0800

    fix: install.sh error

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Run this file for installation!
 
-docker build -f docker/Dockerfile -t aiplay/webserver:v1 --no-cache=true
+docker build -f docker/Dockerfile -t aiplay/webserver:v1 --no-cache=true .
